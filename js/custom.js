83d1484e8dd2c1d761e29dc9387f922ca8cda180
Ranjit Maharjan <Ranjit Maharjan>
2025-03-02 20:09:36 +0545

    project added

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+  $('[data-slick]').slick();
+});
+
+// jQuery(function () {
+//   const slider = jQuery(".team-skin-two-container");
+//   slider;
+
+//   slider.on("wheel", function (e) {
+//     e.preventDefault();
+
+//     if (e.originalEvent.deltaY < 0) {
+//       jQuery(this).slick("slickPrev");
+//     } else {
+//       jQuery(this).slick("slickNext");
+//     }
+//   });
+// });
